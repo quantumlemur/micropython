39bff121a521a8c654716354ec52fec530cfeaea
Mike <quantumlemur@gmail.com>
2020-11-03 09:29:43 -0800

    gitignore current version

--- ports/esp32/modules/currentCommitHash.py
+++ ports/esp32/modules/currentCommitHash.py
@@ -1,2 +1,2 @@
-currentCommitHash = "537cea7f93f28a0608ce70990b364afa570cef46"
-currentCommitTag = "v0.1.0"
+currentCommitHash = "f640c1f42a5d4890020ad9d8d5b6eab665f4d328"
+currentCommitTag = "v0.1.0-1-gf640c1f"
