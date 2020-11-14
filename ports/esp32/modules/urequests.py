096dfe8b127109f3f3683b7ad94a8dbe7daa52dd
Mike <quantumlemur@gmail.com>
2020-11-13 17:52:04 -0800

    urequests investigation

--- ports/esp32/modules/urequests.py
+++ ports/esp32/modules/urequests.py
@@ -1,7 +1,7 @@
 import usocket
 
-class Response:
 
+class Response:
     def __init__(self, f):
         self.raw = f
         self.encoding = "utf-8"
@@ -29,6 +29,7 @@ class Response:
 
     def json(self):
         import ujson
+
         return ujson.loads(self.content)
 
 
@@ -42,6 +43,7 @@ def request(method, url, data=None, json=None, headers={}, stream=None):
         port = 80
     elif proto == "https:":
         import ussl
+
         port = 443
     else:
         raise ValueError("Unsupported protocol: " + proto)
@@ -70,6 +72,7 @@ def request(method, url, data=None, json=None, headers={}, stream=None):
         if json is not None:
             assert data is None
             import ujson
+
             data = ujson.dumps(json)
             s.write(b"Content-Type: application/json\r\n")
         if data:
@@ -77,11 +80,15 @@ def request(method, url, data=None, json=None, headers={}, stream=None):
         s.write(b"\r\n")
         if data:
             s.write(data)
-
-        l = s.readline()
-        #print(l)
-        l = l.split(None, 2)
-        status = int(l[1])
+        try:
+            l = s.readline()
+            # print(l)
+            l = l.split(None, 2)
+            status = int(l[1])
+        except IndexError:
+            print("============================= urequests IndexError")
+            print(l)
+            print(s.readline())
         reason = ""
         if len(l) > 2:
             reason = l[2].rstrip()
@@ -89,7 +96,7 @@ def request(method, url, data=None, json=None, headers={}, stream=None):
             l = s.readline()
             if not l or l == b"\r\n":
                 break
-            #print(l)
+            # print(l)
             if l.startswith(b"Transfer-Encoding:"):
                 if b"chunked" in l:
                     raise ValueError("Unsupported " + l)
@@ -108,17 +115,22 @@ def request(method, url, data=None, json=None, headers={}, stream=None):
 def head(url, **kw):
     return request("HEAD", url, **kw)
 
+
 def get(url, **kw):
     return request("GET", url, **kw)
+
 
 def post(url, **kw):
     return request("POST", url, **kw)
 
+
 def put(url, **kw):
     return request("PUT", url, **kw)
 
+
 def patch(url, **kw):
     return request("PATCH", url, **kw)
+
 
 def delete(url, **kw):
     return request("DELETE", url, **kw)
